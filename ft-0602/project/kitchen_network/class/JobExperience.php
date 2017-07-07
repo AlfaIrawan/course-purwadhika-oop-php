8a621034bef628193c66210209631990ef0750ac
Alfa P. Irawan <alfa.irawan@outlook.com>
2017-07-07 20:35:13 +0700

    Update 20170707.2035

---
+++ ft-0602/project/kitchen_network/class/JobExperience.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alfairawan
+ * Date: 7/7/17
+ * Time: 8:29 PM
+ */
+class JobExperience
+{
+
+}
---
+++ ft-0602/project/kitchen_network/home/index_horeka.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alfairawan
+ * Date: 7/7/17
+ * Time: 8:25 PM
+ */
