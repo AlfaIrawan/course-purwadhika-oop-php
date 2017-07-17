089a1a99c7f20d062352c1d5f5ca87a0af36fecb
Alfa P. Irawan <alfa.irawan@outlook.com>
2017-07-17 19:10:03 +0700

    Update 20170717.1910

---
+++ ft-0602/s02/sample-object-set-01/class/Contact.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alfairawan
+ * Date: 6/21/17
+ * Time: 2:29 PM
+ */
+
+namespace SampleObjectClass01 {
+
+    class Contact
+    {
+
+    }
+
+}
---
+++ ft-0602/s02/sample-object-set-01/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alfairawan
+ * Date: 6/21/17
+ * Time: 4:42 PM
+ */
